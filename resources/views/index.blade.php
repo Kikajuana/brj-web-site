adc1563cc51c87fca003c6e62e1b2a4946d45d07
leregi <leregi46@gmail.com>
2020-10-27 14:41:05 -0400

    menu and hero added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 @section('body')
+    @include('partials.hero')
    <div>
      <h1>Hello world!!!</h1>
    </div> 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,10 +2,19 @@
 <html lang="en">
     @include('layouts.head')
     <body>
-        <div id="wrapper">
-            @include('layouts.header')
-            @yield('body')
-            @include('layouts.footer')
+        <div class="loading-overlay-showing" data-plugin-page-transition data-loading-overlay data-plugin-options="{'hideDelay': 500}">
+            <div class="loading-overlay">
+                <div class="bounce-loader">
+                    <div class="bounce1"></div>
+                    <div class="bounce2"></div>
+                    <div class="bounce3"></div>
+                </div>
+            </div>
+            <div class="body">
+                @include('layouts.header')
+                @yield('body')
+                @include('layouts.footer')
+            </div>
         </div>
         @include('layouts.script')
     </body>
