2bd065aeb2be1575663dfb00513652bc5f1c7a13
leregi <leregi46@gmail.com>
2020-10-27 13:38:45 -0400

    header added

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+@section('body')
+   <div>
+     <h1>Hello world!!!</h1>
+   </div> 
+@endsection
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,9 +3,45 @@
 ================================================== -->
 <title>BRJ</title>
 <meta charset="utf-8">
+<!-- Favicon -->
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <!-- CSS
 ================================================== -->
-<link rel="stylesheet" href="{{ asset('css/theme.css')}}">
+<!-- Web Fonts  -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light%7CPlayfair+Display:400" rel="stylesheet" type="text/css">
+
+<!-- Vendor CSS -->
+<link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/animate/animate.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/simple-line-icons/css/simple-line-icons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/owl.carousel/assets/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/owl.carousel/assets/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/magnific-popup/magnific-popup.min.css') }}">
+
+<!-- Theme CSS -->
+<link rel="stylesheet" href="{{ asset('css/theme.css') }}">
+<link rel="stylesheet" href="{{ asset('css/theme-elements.css') }}">
+<link rel="stylesheet" href="{{ asset('css/theme-blog.css') }}">
+<link rel="stylesheet" href="{{ asset('css/theme-shop.css') }}">
+
+<!-- Current Page CSS -->
+<link rel="stylesheet" href="{{ asset('vendor/rs-plugin/css/settings.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/rs-plugin/css/layers.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/rs-plugin/css/navigation.css') }}">
+
+<!-- Demo CSS -->
+
+
+<!-- Skin CSS -->
+<link rel="stylesheet" href="{{ asset('css/skins/default.css') }}"> 
+
+<!-- Theme Custom CSS -->
 <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+<!-- Head Libs -->
+<script src="{{ asset('vendor/modernizr/modernizr.min.js') }}"></script>
 </head>
