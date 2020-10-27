f200bf09a342d2e48297be04eb19f036d1f4a664
leregi <leregi46@gmail.com>
2020-10-27 16:24:20 -0400

    menu *MON COMPTE* modofied

---
+++ resources/views/partials/vision.blade.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <div class="row pt-5">
+        <div class="col">
+
+            <div class="row text-center pb-5">
+                <div class="col-md-9 mx-md-auto">
+                    <div class="overflow-hidden mb-3">
+                        <h1 class="word-rotator slide font-weight-bold text-8 mb-0 appear-animation animated maskUp appear-animation-visible" data-appear-animation="maskUp" style="animation-delay: 100ms;">
+                            <span>Nous sommes BRJ nous </span>
+                            <span>
+                                <b class="is-visible">apportons</b>
+                            </span>
+                            <span>des solutions</span>
+                        </h1>
+                    </div>
+                    <div class="overflow-hidden mb-3">
+                        <p class="lead mb-0 appear-animation animated maskUp appear-animation-visible" data-appear-animation="maskUp" data-appear-animation-delay="200" style="animation-delay: 200ms;">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elementum, nulla vel pellentesque consequat, ante nulla hendrerit arcu, ac tincidunt mauris lacus sed leo.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-3 mb-5">
+                <div class="col-md-4 appear-animation animated fadeInLeftShorter appear-animation-visible" data-appear-animation="fadeInLeftShorter" data-appear-animation-delay="800" style="animation-delay: 800ms;">
+                    <h3 class="font-weight-bold text-4 mb-2">Notre Mission</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elementum, nulla vel pellentesque consequat, ante nulla hendrerit arcu.</p>
+                </div>
+                <div class="col-md-4 appear-animation animated fadeIn appear-animation-visible" data-appear-animation="fadeIn" data-appear-animation-delay="600" style="animation-delay: 600ms;">
+                    <h3 class="font-weight-bold text-4 mb-2">Notre Vision</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nulla vel pellentesque consequat, ante nulla hendrerit arcu.</p>
+                </div>
+                <div class="col-md-4 appear-animation animated fadeInRightShorter appear-animation-visible" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="800" style="animation-delay: 800ms;">
+                    <h3 class="font-weight-bold text-4 mb-2">Nos Valeurs</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elementum, nulla vel consequat, ante nulla hendrerit arcu.</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
